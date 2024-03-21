dac8cfcc4a754eb01bd2b89dcb332dd7d60b9f43
Hedimar <157844506+Hedimar@users.noreply.github.com>
2024-03-20 21:51:33 -0700

    basic JavaScript Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Hello");//window.alert method
+document.write("Java Script Assignment");//document.write method
+//creating two variables
+var Sent1="This is the beginning of the string";
+var Sent2="This is the end of the string";
+//displaying the variables using document.write
+document.write(Sent1+Sent2);
+//an expression
+document.write(20+25);
