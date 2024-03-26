249e22f3105e121e3b547a5b4a144d5b33a1579d
Hedimar <157844506+Hedimar@users.noreply.github.com>
2024-03-25 21:42:45 -0700

    Project 2

---
+++ Basic JavaScript Projects/Project2_functions/JS/main1.js
@@ -0,0 +1,14 @@
+function My_Function_Assignment(){
+    var str="Button Text!";//Displayed text
+    document.getElementById("Button_Text").innerHTML=str;
+}
+function myFunction(){
+    var sentence="Iam learning"; //This is the variable
+    sentence +=" a lot from this course!";//operator
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
+function myOperator(){
+    var sentence="Iam having fun";
+    sentence += " with javaScript!";
+    document.getElementById("Add").innerHTML=sentence;
+}
